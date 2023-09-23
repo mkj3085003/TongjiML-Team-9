e3a0871be3c17e55b6081cbd5475508d093a47c9
KandS <2922606758@qq.com>
2023-09-23 20:18:23 +0800

    线性回归模型：新增多项式和bias 在有bias的情况下线性能够从0.627提升到0.806，然而三次多项式在有bias的情况下从0.894减少到0.891

--- ml_regression/main.py
+++ ml_regression/main.py
@@ -1,6 +1,5 @@
 from sklearn.metrics import r2_score
-import matplotlib.pyplot as plt
-# from sklearn.linear_model import LinearRegression as lr
+# import matplotlib.pyplot as plt
 
 from ml_regression.preprocess.preprocess import DataPreprocessing
 from model.linearRegression import LinearRegression
@@ -15,18 +14,14 @@ def main():
     X_train, X_test, y_train, y_test = data_processor.preprocess_data(df)
 
     # 在这里可以继续进行模型训练和其他操作
-    linear = LinearRegression(X_train.values, y_train.values)
-    theta1 = linear.train(0.5, 5000)
-    # theta2 = linear.fit()
-    plt.plot(theta1[1])
-    plt.show()
+    linear = LinearRegression(X_train.values, y_train.values, 1, True)
+    # theta1 = linear.train(0.3, 10000)
+    theta2 = linear.fit()
+    # print(theta2)
+    # plt.plot(theta1[1])
+    # plt.show()
     y_test_pred = linear.predict(X_test.values)
     print('Testing accuracy on selected features: %.3f' % r2_score(y_test.values, y_test_pred))
-
-    # model = lr()
-    # model.fit(X_train, y_train)
-    # y_pred = model.predict(X_test)
-    # print(r2_score(y_test, y_pred))
 
 
 if __name__ == "__main__":
