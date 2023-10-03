966e3d3024b0555e60daee1be23e61c20f9de2a6
KandS <2922606758@qq.com>
2023-10-03 22:33:17 +0800

    多项式回归模型： 新增正则化项，由初始化参数regularization开关， lamda可以是常数或向量，在train或fit时传入。

--- ml_regression/model/linearRegression.py
+++ ml_regression/model/linearRegression.py
@@ -3,9 +3,10 @@ from sklearn.preprocessing import PolynomialFeatures
 
 
 class LinearRegression:
-    def __init__(self, data, labels, poly=1, include_bias=True) -> None:
+    def __init__(self, data, labels, poly=1, include_bias=True, regularization=False) -> None:
         self.poly = poly
         self.include_bias = include_bias
+        self.regularization = regularization
 
         # 更新data
         self.data = PolynomialFeatures(degree=self.poly, include_bias=self.include_bias).fit_transform(data)
@@ -15,27 +16,36 @@ class LinearRegression:
         num_features = self.data.shape[1]
         self.theta = np.zeros((num_features, 1))
 
-    def fit(self):
-        self.theta = np.linalg.inv(self.data.T.dot(self.data)).dot(self.data.T).dot(self.labels)
+    def fit(self, lamda=None):
+        if self.regularization:
+            self.theta = np.linalg.inv(self.data.T.dot(self.data) + lamda * np.eye(self.data.shape[1])).dot(
+                self.data.T).dot(self.labels)
+        else:
+            self.theta = np.linalg.inv(self.data.T.dot(self.data)).dot(self.data.T).dot(self.labels)
         return self.theta
 
-    def train(self, alpha, num_iterations=500):
-        cost_history = self.gradient_descent(alpha, num_iterations)
+    def train(self, alpha, num_iterations=500, lamda=None):
+        cost_history = self.gradient_descent(alpha, num_iterations, lamda)
         return self.theta, cost_history
 
-    def gradient_descent(self, alpha, num_iterations):
+    def gradient_descent(self, alpha, num_iterations, lamda=None):
         cost_history = []
         for _ in range(num_iterations):
-            cost = self.gradient_step(alpha)
+            cost = self.gradient_step(alpha, lamda)
             cost_history.append(cost)
         return cost_history
 
-    def gradient_step(self, alpha):
+    def gradient_step(self, alpha, lamda=None):
         num_examples = self.data.shape[0]
         prediction = LinearRegression.hypothesis(self.data, self.theta)
         delta = prediction - self.labels
         theta = self.theta
-        theta = theta - alpha * (1 / num_examples) * (np.dot(delta.T, self.data)).T
+        if self.regularization:
+            theta[1:] = (theta[1:] - alpha * (1 / num_examples) * (np.dot(delta.T, self.data[:, 1:])).T
+                         - alpha * lamda * theta[1:])
+            theta[0] = theta[0] - alpha * (1 / num_examples) * np.sum(delta)
+        else:
+            theta = theta - alpha * (1 / num_examples) * (np.dot(delta.T, self.data)).T
 
         self.theta = theta
 
